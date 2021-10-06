518774b81e2cce4a55db5f344956da214b919581
Sylvester <nnaemekasilver@gmail.com>
2021-10-06 08:29:18 +0100

    Fix linter error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style.css';
 import checkboxesEvent from './status-update';
 import {
   displayTasks, edit, clear, add,
-} from './add-remove.js';
+} from './add-remove';
 
 displayTasks();
 checkboxesEvent();
